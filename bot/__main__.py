8f1fc1dc58197074bb10b6e17df350b0d1b0acf3
Alokfj83077 <108579689+Alokfj83077@users.noreply.github.com>
2022-09-13 09:10:11 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -72,10 +72,10 @@ def getHerokuDetails(h_api_key, h_app_name):
             abc += f"<b>├ 👍🏻 FREE</b>: {get_readable_time(quota_remain)}\n"
         else:
             abc += f'<b></b>\n'
-            abc += f'<b>╭─《 HEROKU STATS 》</b>\n'
-            abc += f"<b>├ FULL</b>: {get_readable_time(account_quota)}\n"
-            abc += f"<b>├ USED</b>: {get_readable_time(quota_used)}\n"
-            abc += f"<b>├ FREE</b>: {get_readable_time(quota_remain)}\n"
+            abc += f'<b>  HEROKU STATS  </b>\n'
+            abc += f"<b>FULL</b>: {get_readable_time(account_quota)}\n"
+            abc += f"<b>USED</b>: {get_readable_time(quota_used)}\n"
+            abc += f"<b>FREE</b>: {get_readable_time(quota_remain)}\n"
         # App Quota
         AppQuotaUsed = 0
         OtherAppsUsage = 0
@@ -100,9 +100,9 @@ def getHerokuDetails(h_api_key, h_app_name):
             abc += f"<b>├ 🗑️ OTHER APP:</b> {get_readable_time(OtherAppsUsage)}\n"
             abc += f'<b>╰─《 ☣️ {CREDIT_NAME} ☣️ 》</b>'
         else:
-            abc += f"<b>├ APP USAGE:</b> {get_readable_time(AppQuotaUsed)}\n"
-            abc += f"<b>├ OTHER APP:</b> {get_readable_time(OtherAppsUsage)}\n"
-            abc += f'<b>╰─《 {CREDIT_NAME} 》</b>'
+            abc += f"<b>APP USAGE:</b> {get_readable_time(AppQuotaUsed)}\n"
+            abc += f"<b>OTHER APP:</b> {get_readable_time(OtherAppsUsage)}\n"
+            abc += f'<b> {CREDIT_NAME} </b>'
         return abc
     except Exception as g:
         LOGGER.error(g)
